f6a82cdf080fbc865e4718b48c94365f4d0a1b0e
Mark <markteams@gmail.com>
2018-08-20 15:36:48 +0300

    config envs added.

---
+++ src/config.py
@@ -0,0 +1,29 @@
+# /src/config.py
+
+import os
+
+class Config(object):
+    """
+    General environment configuration
+    """
+    DEBUG = True
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+
+class Development(Config):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+
+class Production(Config):
+    """
+    Production environment configuration
+    """
+    DEBUG = False
+
+app_config = {
+    'development' : Development,
+    'production' : Production
+}
